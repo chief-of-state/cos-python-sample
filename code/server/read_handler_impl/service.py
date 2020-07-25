5ae997b87b5d24d8620222a67dd3794eccd454ce
Zen Yui <zen.yui@namely.com>
2020-07-25 12:22:45 -0400

    Update service.py

--- code/server/read_handler_impl/service.py
+++ code/server/read_handler_impl/service.py
@@ -1,17 +1,17 @@
 import logging
-from google.protobuf.json_format import MessageToJson
 from chief_of_state.readside_pb2_grpc import ReadSideHandlerServiceServicer
 from chief_of_state.readside_pb2 import HandleReadSideRequest, HandleReadSideResponse
+from cos_helpers.proto import ProtoHelper
 from sample_app.events_pb2 import *
 from sample_app.state_pb2 import *
-from cos_helpers.proto import ProtoHelper
+
 
 logger = logging.getLogger(__name__)
 
 class ReadSideHandlerImpl(ReadSideHandlerServiceServicer):
+    '''read side that logs event JSON to stdout'''
 
     def HandleReadSide(self, request, context):
         logger.info("received event")
-        # logger.info(request)
         logger.info(ProtoHelper.to_json(request))
         return HandleReadSideResponse(successful=True)
